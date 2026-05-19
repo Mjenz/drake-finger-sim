d7db75b7b2da7dde954e26986e66f333bc203ffa
Mjenz <michael.jenz77@gmail.com>
2026-05-19 18:54:58 -0500

    modified urdf that is loaded

--- finger_simulation/finger_simulation/basic_setpoint_sim.py
+++ finger_simulation/finger_simulation/basic_setpoint_sim.py
@@ -133,7 +133,7 @@ class FingerSimulation():
 
         # Weld the grass to the world so that it's fixed during the simulation.
         self.finger, = parser.AddModels(
-            url='package://finger_description/urdf/setpoint_finger.urdf')
+            url='package://finger_description/urdf/setpoint_finger2.urdf')
         self.plant.RenameModelInstance(
             model_instance=self.finger, name='setpoint_finger')
         # parser.AddModels(
